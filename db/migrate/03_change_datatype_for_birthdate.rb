7cf4441cdee566576a5c99badd10f3f635738615
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-02 01:43:39 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
 
   def change
-    change_column(table_name, column_name, type)
+    change_column(:students, column_name, type)
   end
 end
